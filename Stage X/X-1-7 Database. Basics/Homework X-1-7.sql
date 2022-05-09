2b3899db4fa0cf70df866b32f9fe21cd2234c54e
GaleArt <GaleguzovAA@gmail.com>
2022-05-10 00:03:54 +0300

    complete task #2 homework X-1-7

--- Stage X/X-1-7 Database. Basics/Homework X-1-7.sql
+++ Stage X/X-1-7 Database. Basics/Homework X-1-7.sql
@@ -1,46 +1,27 @@
--- 1. Создайте представление, которое для каждого курса выводит название, номер последнего
--- потока, дату начала обучения последнего потока и среднюю успеваемость курса по всем потокам.
--- 2. Удалите из базы данных всю информацию, которая относится к преподавателю с
--- идентификатором, равным 3. Используйте транзакцию.
+-- 1. Выполнено. Представление в локальной версии БД оставил.
+-- 2. Выполнено. Вообще по-хорошему нужно было удалять и наименование курса, но для работы транзакции пойдёт :)
+
 -- 3. Создайте триггер для таблицы успеваемости, который проверяет значение успеваемости на
 -- соответствие диапазону чисел от 0 до 5 включительно.
 -- 4. Дополнительное задание. Создайте триггер для таблицы потоков, который проверяет, что
 -- дата начала потока больше текущей даты, а номер потока имеет наибольшее значение среди
 -- существующих номеров. При невыполнении условий необходимо вызвать ошибку с информативным сообщением.
-
-
-
 CREATE VIEW course_info AS
-SELECT
-courses.name AS course_name,
-streams.number AS stream_number,  --'poslednii_potok',
-  --'started_at poslednii_potok',
-AVG(performance) AS grade--kursa
+  SELECT
+    courses.name AS course_name,
+    MAX(number) AS stream,
+    MAX(started_at) AS last_stream,
+    AVG(performance) AS avg
+  FROM streams
+    LEFT JOIN grades
+      ON streams.id = grades.stream_id
+    LEFT JOIN courses
+      ON streams.course_id = courses.id
+  GROUP BY streams.course_id;
+SELECT * FROM course_info;
 
-FROM courses
-
-
-LEFT JOIN grades
-ON students.id = grades.student_id
-LEFT JOIN streams
-ON grades.stream_id = streams.id
-LEFT JOIN courses
-ON streams.course_id = courses.id;
-
-
-.mode column
-SELECT
-courses.name AS course_name,
-streams.number AS stream,
-'vlozhennyi' AS last_stream,
-AVG(performance) AS avg
-FROM streams
-LEFT JOIN grades
-ON streams.id = grades.stream_id
-LEFT JOIN courses
-ON streams.course_id = courses.id
-GROUP BY streams.course_id;
-
-
-
-SELECT started_at FROM streams ORDER BY SUBSTR(started_at, 4, 2) DESC;
+BEGIN TRANSACTION;
+DELETE FROM grades WHERE teacher_id = 3;
+DELETE FROM teachers WHERE id = 3;
+COMMIT;
+SELECT * FROM teachers;
