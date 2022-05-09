6a0ab709d2de20a181a860e7e58a2baf5d5e82f1
GaleArt <GaleguzovAA@gmail.com>
2022-05-09 23:55:29 +0300

    complete task #1 homework X-1-7

--- Stage X/X-1-7 Database. Basics/Homework X-1-7.sql
+++ Stage X/X-1-7 Database. Basics/Homework X-1-7.sql
@@ -7,3 +7,40 @@
 -- 4. Дополнительное задание. Создайте триггер для таблицы потоков, который проверяет, что
 -- дата начала потока больше текущей даты, а номер потока имеет наибольшее значение среди
 -- существующих номеров. При невыполнении условий необходимо вызвать ошибку с информативным сообщением.
+
+
+
+CREATE VIEW course_info AS
+SELECT
+courses.name AS course_name,
+streams.number AS stream_number,  --'poslednii_potok',
+  --'started_at poslednii_potok',
+AVG(performance) AS grade--kursa
+
+FROM courses
+
+
+LEFT JOIN grades
+ON students.id = grades.student_id
+LEFT JOIN streams
+ON grades.stream_id = streams.id
+LEFT JOIN courses
+ON streams.course_id = courses.id;
+
+
+.mode column
+SELECT
+courses.name AS course_name,
+streams.number AS stream,
+'vlozhennyi' AS last_stream,
+AVG(performance) AS avg
+FROM streams
+LEFT JOIN grades
+ON streams.id = grades.stream_id
+LEFT JOIN courses
+ON streams.course_id = courses.id
+GROUP BY streams.course_id;
+
+
+
+SELECT started_at FROM streams ORDER BY SUBSTR(started_at, 4, 2) DESC;
