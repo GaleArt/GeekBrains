d96b4d1b9de17563a84c9852b8f394a72a6b2a7c
GaleArt <GaleguzovAA@gmail.com>
2022-05-20 11:14:33 +0300

    complete task #2 homework X-1-8

--- Stage X/X-1 Database. Basics/X-1-8 Homework/Homework X-1-8.sql
+++ Stage X/X-1 Database. Basics/X-1-8 Homework/Homework X-1-8.sql
@@ -1,17 +1,29 @@
--- 1-3 задания кодом, 4 - скриншот!!!
---
 -- 1. Выполнено. Оставил еще количество потоков чтобы легче было проверять.
+-- 2. Добавил нового учителя, который еще не провел ни одного занятия для проверки.
+-- Немного не понял проблему: если написать как в методичке, т.е.
+-- WINDOW w_teachers AS (PARTITION BY teachers.id) перед сортировкой и сделать ссылку в OVER, то средний считается по всем учителям
+-- 3.
+-- 4. Скриншот в архиве. Я и до этого пользовался чем-то подобным: "DB Browser (SQLite)" https://sqlitebrowser.org/
+-- 5. Дополнительное задание не делал, не успеваю блин :(
 SELECT DISTINCT
-courses.name,
-COUNT(courses.id) OVER(PARTITION BY streams.course_id) AS course,
-SUM(number) OVER(PARTITION BY courses.id) AS students
-FROM streams
-LEFT JOIN courses
-ON streams.course_id = courses.id
-ORDER BY courses.id;
--- 2. Найдите среднюю оценку по всем потокам для всех учителей. В отчёт выведите идентификатор, фамилию и имя учителя,
--- среднюю оценку по всем проведённым потокам. Учителя, у которых не было потоков, также должны попасть в выборку.
--- Решите задание с применением оконных функций.
+  courses.name AS course_name,
+  COUNT(courses.id) OVER(PARTITION BY streams.course_id) AS amount,
+  SUM(number) OVER(PARTITION BY courses.id) AS students
+  FROM streams
+    LEFT JOIN courses
+      ON streams.course_id = courses.id
+  ORDER BY courses.id;
+
+SELECT DISTINCT
+  teachers.id,
+  teachers.surname,
+  teachers.name,
+  AVG(performance) OVER(PARTITION BY teachers.id) AS GPA
+  FROM teachers
+    LEFT JOIN grades
+      ON grades.teacher_id = teachers.id
+  ORDER BY teachers.surname, teachers.name;
+
 -- 3. Какие индексы надо создать для максимально быстрого выполнения представленного запроса?
 --
 -- SELECT
@@ -25,10 +37,3 @@ ORDER BY courses.id;
 -- JOIN streams
 -- ON academic_performance.stream_id = streams.id
 -- WHERE number >= 200;
---
--- 4. Установите SQLiteStudio, подключите базу данных учителей, выполните в графическом клиенте любой запрос.
-
--- 5. Дополнительное задание. Для каждого преподавателя выведите имя, фамилию, минимальное значение успеваемости
--- по всем потокам преподавателя, название курса, который соответствует потоку с минимальным значением успеваемости, максимальное
--- значение успеваемости по всем потокам преподавателя, название курса, соответствующий потоку с максимальным значением успеваемости.
--- Выполните задачу с использованием оконных функций.
