71c52c017ca98a763120f5cbcb4e4f7ddd8d80ca
GaleArt <GaleguzovAA@gmail.com>
2022-05-30 21:15:42 +0300

    complete homework #1-3-4

---
+++ Stage 1/1-3 Professional layout/1-3-4 Homework/homework.js
@@ -0,0 +1,88 @@
+function showMenu() {
+    document.getElementById('menu').style.display = "block";
+ }
+
+function closeMenu() {
+    document.getElementById('menu').style.display = "none";
+}
+
+/* Тут копипаст кода карусели для страницы Product */
+(function() {
+	"use strict";
+
+	function Carousel(setting) {
+		if(document.querySelector(setting.wrap) === null) {
+			console.error(`Carousel not fount selector ${setting.wrap}`);
+			return;
+		}
+
+		let privates = {};
+
+		this.prev_slide = () => {
+			--privates.opt.position;
+
+			if(privates.opt.position < 0) {
+				privates.sel.wrap.classList.add('s-notransition');
+				privates.opt.position = privates.opt.max_position - 1;
+			}
+			privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.position}00%)`;
+		};
+
+		this.next_slide = () => {
+			++privates.opt.position;
+
+			if(privates.opt.position >= privates.opt.max_position) {
+				privates.opt.position = 0;
+			}
+
+			privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.position}00%)`;
+		};
+
+		privates.setting = setting;
+
+		privates.sel = {
+			"main": document.querySelector(privates.setting.main),
+			"wrap": document.querySelector(privates.setting.wrap),
+			"children": document.querySelector(privates.setting.wrap).children,
+			"prev": document.querySelector(privates.setting.prev),
+			"next": document.querySelector(privates.setting.next)
+		};
+
+		privates.opt = {
+			"position": 0,
+			"max_position": document.querySelector(privates.setting.wrap).children.length
+		};
+
+		if(privates.sel.prev !== null) {
+			privates.sel.prev.addEventListener('click', () => {
+				this.prev_slide();
+			});
+		}
+
+		if(privates.sel.next !== null) {
+			privates.sel.next.addEventListener('click', () => {
+				this.next_slide();
+			});
+		}
+	}
+
+	let a = new Carousel({
+		"main": ".product__carousel__area",
+		"wrap": ".product__carousel__wrap",
+		"prev": ".product__carousel__btn__prev",
+		"next": ".product__carousel__btn__next"
+	});
+
+})();
+
+/* Вешаем счетчик для корзины */
+const btn = document.querySelector('.product__collection__btn');
+const display = document.querySelector('.menu__basket-count');
+
+function basketCounter(event) {
+  event.preventDefault();
+  let counter = display.textContent;
+  display.textContent = parseInt(counter) + 1;
+}
+
+btn.addEventListener('click', basketCounter);
